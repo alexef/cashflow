cbcf8a53e6914628821660d987c15d258b3be762
Alex Eftimie <alex@eftimie.ro>
2013-02-03 16:15:26 +0200

    Refactor view, improve get_parent api.

--- cashflow.py
+++ cashflow.py
@@ -12,7 +12,7 @@ url_mappings = (
     webapp2.Route(r'/wallet/', handler=WalletsPage, name='wallets'),
     webapp2.Route(r'/wallet/<id:\d+>/', handler=WalletTransactions, name='wallet'),
     webapp2.Route(r'/wallet/<id:\d+>/transaction/add/', handler=WalletTransactionAdd, name='wallet-transaction-add'),
-    webapp2.Route(r'/transaction/add/', handler=AddTransaction, name='transaction-add'),
+    webapp2.Route(r'/transaction/add/', handler=TransactionAdd, name='transaction-add'),
     webapp2.Route(r'/transaction/<id:\d+>/delete/', handler=TransactionDelete, name='transaction-del'),
     webapp2.Route(r'/categories', handler=CategoriesPage, name='categories'),
     webapp2.Route(r'/categories/<id:\d+>/', handler=CategoryPage, name='category'),
--- views.py
+++ views.py
@@ -3,64 +3,73 @@ from models import Transaction, Wallet, Category, get_account_ancestor
 from base import AuthenticatedBaseHandler
 
 
-class MainPage(AuthenticatedBaseHandler):
+class MainHandler(AuthenticatedBaseHandler):
+    def get_parent(self):
+        return get_account_ancestor(self.user)
+
+
+class MainPage(MainHandler):
     def get(self):
-        transactions = Transaction.all().ancestor(get_account_ancestor(self.user)).order('-date')
+        transactions = Transaction.all().ancestor(self.get_parent()).order('-date')
         balance = sum([t.amount for t in transactions])
-        template_values = {'transactions': transactions, 'categories': Category.all().ancestor(get_account_ancestor(self.user)),
-                           'balance': balance, 'wallets': Wallet.all().ancestor(get_account_ancestor(self.user))}
+        template_values = {'transactions': transactions, 'categories': Category.all().ancestor(self.get_parent()),
+                           'balance': balance, 'wallets': Wallet.all().ancestor(self.get_parent())}
         self.render_to_response('index.html', template_values)
 
 
-class WalletTransactions(AuthenticatedBaseHandler):
+class WalletTransactions(MainHandler):
     def get(self, id):
-        wallet = Wallet.get_by_id(int(id), parent=get_account_ancestor(self.user))
+        wallet = Wallet.get_by_id(int(id), parent=self.get_parent())
         template_values = {'transactions': wallet.transaction_set,
-                           'wallets': Wallet.all().ancestor(get_account_ancestor(self.user)),
+                           'wallets': Wallet.all().ancestor(self.get_parent()),
                            'wallet': wallet,
-                           'categories': Category.all().ancestor(get_account_ancestor(self.user)),
+                           'categories': Category.all().ancestor(self.get_parent()),
                            'balance': sum([t.amount for t in wallet.transaction_set]),
                            }
         self.render_to_response('wallet.html', template_values)
 
 
-class WalletTransactionAdd(AuthenticatedBaseHandler):
+class WalletTransactionAdd(MainHandler):
     def post(self, id):
-        wallet = Wallet.get_by_id(int(id), parent=get_account_ancestor(self.user))
+        wallet = Wallet.get_by_id(int(id), parent=self.get_parent())
         amount = int(self.request.get('amount'))
         category = self.request.get('category')
         description = self.request.get('description')
         category_object = Category.gql("WHERE name = :1", category).get()
         transaction = Transaction(amount=amount, category=category_object, description=description,
-            wallet=wallet, parent=get_account_ancestor(self.user))
+            wallet=wallet, parent=self.get_parent())
         transaction.put()
         self.redirect(self.uri_for('wallet', id=wallet.id))
 
 
-class WalletsPage(AuthenticatedBaseHandler):
+class WalletsPage(MainHandler):
     def get(self):
-        self.render_to_response('wallets.html', {'wallets': Wallet.all().ancestor(get_account_ancestor(self.user))})
+        self.render_to_response('wallets.html', {'wallets': Wallet.all().ancestor(self.get_parent())})
 
     def post(self):
         name = self.request.get('name')
-        wallet = Wallet.all().ancestor(get_account_ancestor(self.user)).filter('name', name).get()
+        wallet = Wallet.all().ancestor(self.get_parent()).filter('name', name).get()
         if wallet is None:
-            Wallet(name=name, parent=get_account_ancestor(self.user)).put()
+            Wallet(name=name, parent=self.get_parent()).put()
         self.get()
 
 
-class AddTransaction(AuthenticatedBaseHandler):
+class TransactionAdd(MainHandler):
     def post(self):
         amount = int(self.request.get('amount'))
         category = self.request.get('category')
+        wallet = self.request.get('wallet')
         description = self.request.get('description')
         category_object = Category.gql("WHERE name = :1", category).get()
-        transaction = Transaction(amount=amount, category=category_object, description=description, parent=get_account_ancestor(self.user))
+        wallet_object = Wallet.gql("WHERE name = :1", wallet).get()
+        transaction = Transaction(amount=amount, category=category_object,
+                                  wallet=wallet_object, description=description,
+                                  parent=self.get_parent())
         transaction.put()
         self.redirect('/')
 
 
-class TransactionDelete(AuthenticatedBaseHandler):
+class TransactionDelete(MainHandler):
     def get(self, id):
         transaction = Transaction.get_by_id(int(id))
         template_values = {'object': transaction}
@@ -72,43 +81,42 @@ class TransactionDelete(AuthenticatedBaseHandler):
         self.redirect(self.uri_for('home'))
 
 
-class CategoriesPage(AuthenticatedBaseHandler):
+class CategoriesPage(MainHandler):
     def post(self):
         name = self.request.get('name')
-        category = Category.gql("WHERE ANCESTOR IS :1 AND name = :2", get_account_ancestor(self.user), name).get()
+        category = Category.gql("WHERE ANCESTOR IS :1 AND name = :2", self.get_parent(), name).get()
         if category is None:
-            Category(name=name, parent=get_account_ancestor(self.user)).put()
+            Category(name=name, parent=self.get_parent()).put()
         self.get()
 
     def get(self):
-        categories = Category.all().ancestor(get_account_ancestor(self.user))
+        categories = Category.all().ancestor(self.get_parent())
         template_values = {'categories': categories}
         self.render_to_response('categories.html', template_values)
 
 
-class CategoryPage(AuthenticatedBaseHandler):
+class CategoryPage(MainHandler):
     def get(self, id):
-        category = Category.get_by_id(int(id), parent=get_account_ancestor(self.user))
-        transactions = Transaction.gql("WHERE category = :1", category)
-        template_values = {'category': category, 'transactions': transactions}
+        category = Category.get_by_id(int(id), parent=self.get_parent())
+        template_values = {'category': category, 'transactions': category.transaction_set}
         self.render_to_response('category.html', template_values)
 
     def post(self, id):
-        category = Category.get_by_id(int(id), parent=get_account_ancestor(self.user))
+        category = Category.get_by_id(int(id), parent=self.get_parent())
         if category:
             category.name = self.request.get('name')
             category.put()
         self.redirect(self.uri_for('category', id=category.id))
 
 
-class CategoryDeletePage(AuthenticatedBaseHandler):
-    def get(self, category):
-        category = Category.gql("WHERE name = :1", category).get()
+class CategoryDeletePage(MainHandler):
+    def get(self, id):
+        category = Category.get_by_id(int(id), parent=self.get_parent())
         template_values = {'object': category}
         self.render_to_response('delete.html', template_values)
 
-    def post(self, category):
-        category = Category.gql("WHERE name = :1", category).get()
+    def post(self, id):
+        category = Category.get_by_id(int(id), parent=self.get_parent())
         if category:
             db.delete(category.transaction_set)
             category.delete()
